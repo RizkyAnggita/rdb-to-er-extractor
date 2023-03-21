525ae4f6d8446a7d5b7294376ce55b36d57beec0
RizkyAnggita <13519132@std.stei.itb.ac.id>
2023-03-21 12:24:49 +0700

    Rename filename

--- inclusion/generate_inclusion_dependencies.go
+++ inclusion/generate_inclusion_dependencies.go
@@ -6,7 +6,7 @@ import (
 )
 
 func HeuristicSupertypeRelationship(arrTable []model.Table) (res []model.InclusionDependency) {
-
+	// Heuristic 5.2.1 point 1
 	for i := 0; i < len(arrTable); i++ {
 		pks := arrTable[i].PrimaryKeys
 		if arrTable[i].Type == "STRONG" {
@@ -31,7 +31,8 @@ func HeuristicSupertypeRelationship(arrTable []model.Table) (res []model.Inclusi
 	return res
 }
 
-func HeuristicByForeignKey(arrTable []model.Table) (res []model.InclusionDependency) {
+func HeuristicRelationshipByForeignKey(arrTable []model.Table) (res []model.InclusionDependency) {
+	// Heuristic 5.2.1 point 2
 	lenTable := len(arrTable)
 	for i := 0; i < lenTable; i++ {
 		relationType := arrTable[i].Type
@@ -39,7 +40,7 @@ func HeuristicByForeignKey(arrTable []model.Table) (res []model.InclusionDepende
 			pks := arrTable[i].PrimaryKeys
 			for j := 0; j < lenTable; j++ {
 				for _, pk := range pks {
-					if isExistInFk := helper.IsExistInForeignKeys(pk.ColumnName, arrTable[j].ForeignKeys); isExistInFk && arrTable[i].Name != arrTable[j].Name {
+					if helper.IsExistInForeignKeys(pk.ColumnName, arrTable[j].ForeignKeys) && arrTable[i].Name != arrTable[j].Name {
 						res = append(res, model.InclusionDependency{
 							RelationAName: arrTable[j].Name,
 							RelationBName: arrTable[i].Name,
@@ -56,6 +57,34 @@ func HeuristicByForeignKey(arrTable []model.Table) (res []model.InclusionDepende
 									KeyB:          pk.ColumnName,
 								})
 							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func HeuristicRelationShipOwnerAndParticipatingEntity(arrTable []model.Table) (res []model.InclusionDependency) {
+	// Heuristic 5.2.1 point 1
+	lenTable := len(arrTable)
+
+	for i := 0; i < lenTable; i++ {
+		if arrTable[i].Type != "STRONG" {
+			pks := arrTable[i].PrimaryKeys
+			for j := 0; j < lenTable; j++ {
+				jType := arrTable[j].Type
+				if arrTable[i].Name != arrTable[j].Name && (jType == "STRONG" || jType == "WEAK") {
+					for _, pk := range pks {
+						if helper.IsExistInPrimaryKeys(pk.ColumnName, arrTable[j].PrimaryKeys) {
+							res = append(res, model.InclusionDependency{
+								RelationAName: arrTable[i].Name,
+								RelationBName: arrTable[j].Name,
+								KeyA:          pk.ColumnName,
+								KeyB:          pk.ColumnName,
+							})
 						}
 					}
 				}
