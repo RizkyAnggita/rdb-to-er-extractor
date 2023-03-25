c980fc7665d6801515a8cbb53f23d84eb2b49a63
RizkyAnggita <13519132@std.stei.itb.ac.id>
2023-03-25 14:19:29 +0700

    Adjust Endpoint to receive correct parameters from FE

--- main.go
+++ main.go
@@ -16,32 +16,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.JSON(http.StatusCreated, newAlbum)
+type ExtractERParams struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	DBName   string `json:"dbName"`
+	Port     string `json:"port"`
+	URL      string `json:"url"`
 }
 
 func main() {
@@ -49,8 +29,7 @@ func main() {
 	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
 	router.Use(cors.New(config))
-	router.POST("/albums", postAlbums)
-	router.GET("/albums", convertRDBtoEERModel)
+	router.POST("/extract-eer", convertRDBtoEERModel)
 
 	// fmt.Println("RES: ", res)
 
@@ -58,16 +37,21 @@ func main() {
 }
 
 func convertRDBtoEERModel(c *gin.Context) {
-	username := "root"
-	password := "regars2000"
-	url := "localhost"
-	port := "3306"
+	var params ExtractERParams
+	if err := c.ShouldBindJSON(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	username := params.Username
+	password := params.Password
+	url := params.URL
+	port := params.Port
 	dbType := "mysql"
-	dbName := "my_school"
+	dbName := params.DBName
 
 	db, err := sql.Open(dbType, username+":"+password+"@tcp("+url+":"+port+")/"+dbName)
 	if err != nil {
-		panic(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error: ": err.Error()})
 	}
 
 	defer db.Close()
